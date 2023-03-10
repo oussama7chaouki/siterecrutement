5f713fe687e501c1fad4e9ea0a9024d7e6e9ab8b
Oussama Chaouki <oussama11chaouki@gmail.com>
2023-03-11 00:12:34 +0100

    tired

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <script defer src="hicham.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<header>
+
+ <a href="#" class="logo">LOGO</a>
+ <nav class="nav">
+
+ <a href="#login">Recruter</a>
+ <a href="#signup">Candidat</a>
+ 
+ </nav>
+
+</header>
+
+<section class="main">
+
+<div class="content">
+<h2 id="oussamach">find a <span id="och" class="grish">job </span> that<br><span class="grish">matches</span> your <span id="ouch">  passion</span></h2>
+<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque praesentium aut ex incidunt porro voluptas repellat.</h3>
+
+<div class="row">
+
+    <button data-toggle="modal" data-target="#exampleModal" class="card1">SIGN IN</button>
+        
+
+    <button data-toggle="modal" data-target="#exampleModal"  class="card1">SIGN UP</button>
+
+</div>
+
+
+</div>
+</section>
+<!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#C4F2FF" fill-opacity="1" d="M0,160L60,165.3C120,171,240,181,360,197.3C480,213,600,235,720,229.3C840,224,960,192,1080,165.3C1200,139,1320,117,1380,106.7L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
+  </svg> -->
+<!----------2end section---->
+<section class="main2">
+
+
+    <div>
+       
+
+        </div>
+
+        <h2>why we are different?</h2>
+        <h3>make the choosing process<br>
+            faster  and easier for both side<br>
+            by calculating the candidate score <br>
+           and parsing data from the cv doc directly</h3><br>
+           
+           <img src="hicham.png" width="600" height="500" style="float:right; ">
+
+    </div>
+
+   
+
+
+</section>
+
+
+
+<!--footer----->
+<footer>
+ <h4>© 2022 Jobs Portal. </h4>
+
+<div class="foot">
+    <a href="#">CONTACT US</a>
+    <a href="#">LOGIN</a>
+    <a href="#">SIGN UP</a>
+
+</div>
+
+
+
+
+</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
